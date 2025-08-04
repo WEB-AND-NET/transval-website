4a3211ac1581a6448027f2a747b34c5b6c5c1e41
jhonatan-martinez-coder <desarrolladorwebandnet6@gmail.com>
2025-08-04 15:49:26 -0500

    feat: add hero section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,7 +18,19 @@
     </div>
     <!-- Navbar End -->
 
-    
+    <!-------- Hero ---------->
+    <div class="hero-section" style="background-image: url({{ asset('img/carousel/carousel-image-1.webp') }}); ">
+        <div class="col-sm-12 col-md-5 bg-primary h-100" style="padding: 8rem 6rem 4rem 8rem; opacity: 0.8;">
+            <div class="d-flex align-items-center">
+                <div style="color: #ffffff">
+                    <h1 style="color: #ffffff">SOLUCIONES <br>EN COMERCIO <br>EXTERIOR</h1>
+                    <p class="pt-3">Expertos en operaciones logísticas y <br>sistemas de gestión.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-------- Hero ---------->
+
 
     <!---------- JAVASCRIPT ---------->
     @include('partials.footer')
